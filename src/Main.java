c01781f380e6e31ce39bcbc554bc480f8f8ee424
Madina <madina-khat@yandex.ru>
2022-10-19 16:55:50 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+       public static void main(String[] args) {
+           int[] arr = new int [3];
+           double [] weight = {1.57, 7.654, 9.986};
+           short [] small = {135, 225, 315};
+           System.out.println(arr.length);
+           System.out.println(weight.length);
+           System.out.println(small.length);
+
+           }}
